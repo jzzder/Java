1fd78ff8438e2bf44e8eba3674078cad8670893e
Jaz <145226018+jzzder@users.noreply.github.com>
2026-08-19 21:38:42 -0600

    Data Types

---
+++ course/DataTypes.java
@@ -0,0 +1,40 @@
+package course;
+
+/**
+ * DataTypes
+ * primitive: integer, float, character, boolean
+ * 
+ */
+public class DataTypes {
+
+    public static void main(String[] args) {
+        // int num1 = 3;
+        // int num2 = 10;
+        // int result = (num1 + num2) * 2;
+
+
+        //  'float' uses 32 bits (4 bytes) and requires an 'f' suffix.
+        // It provides ~6-7 decimal digits of precision
+        
+        
+        //float singlePrecision = 9.8f;
+
+        // 'double' useas 64 bits (8 bytes) and is Java's default for floating-point numbers.
+        // It provides ~15-16 decimal digits of precision
+        // double doublePrecision = 9.8;
+
+        // char c = 'j';
+        // boolean b = true;
+
+        // System.out.println("Float: " + singlePrecision);
+        // System.out.println("Double: " + doublePrecision);
+
+        // System.out.println(c);
+        // System.out.println(b);
+
+        // literals
+        int num1 = 0b101;
+        System.out.println(num1);
+
+    }
+}
