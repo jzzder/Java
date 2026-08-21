9d0e70a286f472881b72b98aae5ff61da2d0b7fb
Jaz <145226018+jzzder@users.noreply.github.com>
2026-08-20 21:17:48 -0600

    Conditional Statements

---
+++ course/ConditionalStatements.java
@@ -0,0 +1,13 @@
+package course;
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        int x = 18;
+
+         // if(x > 10)
+            // System.out.println("Hola Jazmin");
+
+        if (x > 10 && x <=20)
+            System.out.println("Hola");
+    }
+}
