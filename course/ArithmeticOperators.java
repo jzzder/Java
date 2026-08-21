77b1e1c8fe986102d262c959f6008216901fb48f
Jaz <145226018+jzzder@users.noreply.github.com>
2026-08-20 20:55:58 -0600

    Arithmetic Operators

---
+++ course/ArithmeticOperators.java
@@ -0,0 +1,17 @@
+package course;
+
+public class ArithmeticOperators {
+    
+    public static void main(String[] args) {
+        // int x = 20;
+        // int y = 9;
+        // int y = 0;
+        // int y = 20;
+        // boolean restult = x != y;
+
+        double x = 10.2;
+        double y = 2.2;
+        boolean result = x <= y;
+        System.out.println(result);
+    }
+}
